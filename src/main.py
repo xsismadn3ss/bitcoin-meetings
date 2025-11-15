502f0717a9febaba1ba410c50ae97169b46bf093
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-15 17:12:49 -0600

    💄  importar esquema de colores para la aplicación

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import flet as ft
+from config.theme import scheme
 from service.router import Router
 
 
 def main(page: ft.Page):
     page.title = "Bitcoin Meetings"
+
+    page.theme = ft.Theme(color_scheme=scheme)
+
     router = Router(page=page)
     page.on_route_change = router.route_change
     page.on_view_pop = router.view_pop
