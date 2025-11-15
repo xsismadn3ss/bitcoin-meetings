1879380e83ee096dabcc1cd146c127cc23d11f19
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-15 17:12:12 -0600

    💄  crear esquema de colores para la aplicación

---
+++ src/config/theme.py
@@ -0,0 +1,10 @@
+from flet import ColorScheme, Colors
+
+scheme = ColorScheme(
+    primary=Colors.ORANGE,
+    secondary=Colors.ORANGE_ACCENT,
+    primary_container=Colors.ORANGE,
+    secondary_container=Colors.ORANGE_ACCENT,
+    primary_fixed=Colors.ORANGE,
+    secondary_fixed=Colors.ORANGE,
+)
