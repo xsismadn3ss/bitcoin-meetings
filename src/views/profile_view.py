69243e830b2bc7ffc8b4b295b1dd9d0364b54ef6
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-15 17:33:42 -0600

    🌱  crear página para perfil

---
+++ src/views/profile_view.py
@@ -0,0 +1,17 @@
+import flet as ft
+from .layout_view import LayoutView
+
+
+class ProfileView(LayoutView):
+    def __init__(self, page: ft.Page, route: str = "/profile"):
+        super().__init__(
+            page=page,
+            route=route,
+            controls=[
+                ft.Column(
+                    [ft.Text("Perfil", size=28, weight=ft.FontWeight.BOLD)],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                ),
+            ],
+        )
