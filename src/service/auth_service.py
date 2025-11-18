164ced53a276c14a6d084cf17e0cabf85e3c77b4
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-18 17:11:32 -0600

    👔  implementar endpoint para iniciar sesión

---
+++ src/service/auth_service.py
@@ -0,0 +1,17 @@
+import httpx
+from config.api import *
+from models.auth import AuthLoginDto
+
+
+async def login(email, password) -> dict:
+
+    data = AuthLoginDto(email=email, password=password)
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{PREFIX}/auth/login",
+            json=data.model_dump(),
+            timeout=10.0,
+        )
+        response.raise_for_status()
+        return response.json()
