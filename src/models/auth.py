bcddbcc00a6a60f6f2bb5d0340377db7281cc6d5
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-18 22:09:26 -0600

    ♻️  agregar validación personalizada para email

--- src/models/auth.py
+++ src/models/auth.py
@@ -1,6 +1,24 @@
-from pydantic import BaseModel, Field, EmailStr
+from typing import Any
+from pydantic import BaseModel, Field, field_validator
+import re
+
+EMAIL_REGEX = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 
 
 class AuthLoginDto(BaseModel):
-    email: EmailStr = Field(..., description="Correo electrónico valido")
-    password: str = Field(..., min_length=6, max_length=128)
+    email: str = Field(..., description="Correo electrónico valido")
+    password: str = Field(
+        ...,
+        min_length=6,
+        max_length=128,
+        json_schema_extra={
+            "error_message": "La contraseña debe tener entre 6 y 128 carácteres"
+        },
+    )
+
+    @field_validator("email")
+    @classmethod
+    def validate(cls, value: Any):
+        if not re.match(EMAIL_REGEX, value):
+            raise ValueError("El formato de correo electrónico es inválido")
+        return value
