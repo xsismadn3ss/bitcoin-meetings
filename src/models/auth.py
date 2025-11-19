2a9828f72e6b0c50a478e4bc1a63b4772105afb7
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-18 22:17:17 -0600

    ♻️  abstraer lógica e importar utilidad para validar email

--- src/models/auth.py
+++ src/models/auth.py
@@ -1,8 +1,6 @@
 from typing import Any
 from pydantic import BaseModel, Field, field_validator
-import re
-
-EMAIL_REGEX = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+from utils.email import validate_email
 
 
 class AuthLoginDto(BaseModel):
@@ -19,6 +17,6 @@ class AuthLoginDto(BaseModel):
     @field_validator("email")
     @classmethod
     def validate(cls, value: Any):
-        if not re.match(EMAIL_REGEX, value):
+        if not validate_email(value):
             raise ValueError("El formato de correo electrónico es inválido")
         return value
